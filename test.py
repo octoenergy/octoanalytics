2e6f3367073fe8263e0d5591724d1a6c158b7c18
Jean Bertin <jean.bertin@octopusenergy.fr>
2025-05-09 22:06:57 +0200

    first commit on main

---
+++ test.py
@@ -0,0 +1,10 @@
+# test_spot_price_script.py
+from octoanalytics import get_spot_prices
+
+
+country = "FR"
+start_date = "2025-04-24"
+end_date = "2025-04-25"
+
+
+
---
+++ tests/test_core.py
@@ -0,0 +1,5 @@
+from octoanalytics import eval_forecast
+
+
+
+
